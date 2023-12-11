c2483530dd1c28a4f6ffef2667ef68a36db171eb
Matteo Voges <98756476+MatteoVoges@users.noreply.github.com>
2023-12-11 03:49:23 +0100

    feat: index bug / phi-theta / resolution / shading / normals (#7)
    * wip
    
    * wip
    
    * wip

--- src/superquadricGeometry.js
+++ src/superquadricGeometry.js
@@ -1,21 +1,36 @@
-import { BufferGeometry, Vector3, Float32BufferAttribute } from 'three';
+import { BufferGeometry, Vector3, Float32BufferAttribute } from "three";
 
 class SuperquadricGeometry extends BufferGeometry {
-
-	constructor(widthSegments = 32, heightSegments = 16, epsilon_1 = 1, epsilon_2 = 1) {
-
+	constructor(
+		epsilon_1 = 1.0,
+		epsilon_2 = 1.0,
+		widthSegments = 32,
+		heightSegments = 16,
+		phiStart = 0,
+		phiLength = Math.PI * 2,
+		thetaStart = 0,
+		thetaLength = Math.PI
+	) {
 		super();
 
-		this.type = 'SuperquadricGeometry';
+		this.type = "SuperquadricGeometry";
 
 		this.parameters = {
+			epsilon_1: epsilon_1,
+			epsilon_2: epsilon_2,
 			widthSegments: widthSegments,
 			heightSegments: heightSegments,
+			phiStart: 0,
+			phiLength: Math.PI * 2,
+			thetaStart: 0,
+			thetaLength: Math.PI,
 		};
 
-		widthSegments = Math.max(3, Math.floor(widthSegments));
-		heightSegments = Math.max(2, Math.floor(heightSegments));
+		widthSegments = Math.max(4, Math.floor(widthSegments));
+		heightSegments = Math.max(3, Math.floor(heightSegments));
 
+		const thetaEnd = Math.min( thetaStart + thetaLength, Math.PI );
+		
 		let index = 0;
 		const grid = [];
 
@@ -28,16 +43,22 @@ class SuperquadricGeometry extends BufferGeometry {
 		// generate vertices
 
 		for (let iy = 0; iy <= heightSegments; iy++) {
-
 			const verticesRow = [];
 
 			const v = iy / heightSegments;
-			const eta = - Math.PI/2 - v * Math.PI;
+			const eta = thetaStart - Math.PI/2 + v * thetaLength;
 
 			for (let ix = 0; ix <= widthSegments; ix++) {
+				const u = (ix+1) / widthSegments;
+				const omega = phiStart + u * phiLength;
 
-				const u = ix / widthSegments;
-				const omega = u * 2 * Math.PI;
+				// handle poles correctly
+				if (v == 0 || v == 1) {
+					vertices.push(0, -1 + v * 2, 0);
+					normals.push(0, -1 + v * 2, 0);
+					verticesRow.push(index++);
+					continue;
+				}
 
 				// vertex
 				let vertex = new Vector3();
@@ -46,16 +67,18 @@ class SuperquadricGeometry extends BufferGeometry {
 				vertex.z = cos_epsilon(eta, epsilon_1) * sin_epsilon(omega, epsilon_2);
 
 				vertices.push(vertex.x, vertex.y, vertex.z);
-				verticesRow.push(index++);
-
+				
+				// normal
 				let normal = new Vector3();
-				normal.x = cos_epsilon(eta, 2-epsilon_1) * cos_epsilon(omega, 2-epsilon_2);
-				normal.y = sin_epsilon(eta, 2-epsilon_1);
-				normal.z = cos_epsilon(eta, 2-epsilon_1) * sin_epsilon(omega, 2-epsilon_2);
-
+				normal.x = cos_epsilon(eta, 2 - epsilon_1) * cos_epsilon(omega, 2 - epsilon_2);
+				normal.y = sin_epsilon(eta, 2 - epsilon_1);
+				normal.z = cos_epsilon(eta, 2 - epsilon_1) * sin_epsilon(omega, 2 - epsilon_2);
+				
 				normals.push(normal.x, normal.y, normal.z);
+
+				verticesRow.push(index++);
 			}
-			verticesRow.push(verticesRow.length - 1 - widthSegments);
+
 			grid.push(verticesRow);
 		}
 
@@ -70,20 +93,46 @@ class SuperquadricGeometry extends BufferGeometry {
 				const c = grid[iy + 1][ix];
 				const d = grid[iy + 1][ix + 1];
 
-				if (iy !== 0)
-					indices.push(a, b, d);
-				if (iy !== heightSegments - 1)
-					indices.push(b, c, d);
+				if ( iy == heightSegments - 1 ) {
+					if (thetaEnd == Math.PI)
+						indices.push( a, b, d );
+				} else if ( iy == 0) {
+					if (thetaStart == 0)
+						indices.push( b, c, d );
+				} else {
+					indices.push( a, b, d );
+					indices.push( b, c, d );
+				}
 			}
 		}
 
 		// build geometry
 
 		this.setIndex(indices);
-		this.setAttribute('position', new Float32BufferAttribute(vertices, 3));
-		this.setAttribute( 'normal', new Float32BufferAttribute( normals, 3 ) );
+		this.setAttribute("position", new Float32BufferAttribute(vertices.flat(), 3));
+		this.setAttribute("normal", new Float32BufferAttribute(normals, 3));
 	}
 
+	copy(source) {
+		super.copy(source);
+
+		this.parameters = Object.assign({}, source.parameters);
+
+		return this;
+	}
+
+	static fromJSON(data) {
+		return new SuperquadricGeometry(
+			data.epsilon_1,
+			data.epsilon_2,
+			data.widthSegments,
+			data.heightSegments,
+			data.phiStart,
+			data.phiLength,
+			data.thetaStart,
+			data.thetaLength
+		);
+	}
 }
 
 export { SuperquadricGeometry };
