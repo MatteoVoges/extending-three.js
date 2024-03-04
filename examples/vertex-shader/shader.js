782818e4f58fcf53942f619e1afce0714fdaa2fd
Matteo Voges <98756476+MatteoVoges@users.noreply.github.com>
2024-03-04 03:37:51 +0100

    fix vertex shader edge case with u buffer

--- examples/vertex-shader/shader.js
+++ examples/vertex-shader/shader.js
@@ -4,10 +4,12 @@ uniform float epsilon_2;
 
 attribute float eta;
 attribute float omega;
+attribute float u;
 
 varying vec3 vViewPosition;
 varying vec3 vNormal;
 varying vec3 vWorldPosition;
+
 
 const float tolerance = 1e-15;
 
@@ -20,25 +22,33 @@ float signed_pow(float x, float y) {
 }
 
 void main() {
+
+    // updated position
     
-    vec3 newPosition;
-    newPosition.x = -signed_pow(sin(eta), epsilon_1) * signed_pow(cos(omega), epsilon_2);
-    newPosition.y = signed_pow(cos(eta), epsilon_1);
-    newPosition.z = signed_pow(sin(eta), epsilon_1) * signed_pow(sin(omega), epsilon_2);
+    vec3 superquadricPosition;
+    superquadricPosition.x = -signed_pow(sin(eta), epsilon_1) * signed_pow(cos(omega), epsilon_2);
+    superquadricPosition.y = signed_pow(cos(eta), epsilon_1);
+    superquadricPosition.z = signed_pow(sin(eta), epsilon_1) * signed_pow(sin(omega), epsilon_2);
+    if (u == 1.0) superquadricPosition.z = 0.0;
 
-    gl_Position = projectionMatrix * modelViewMatrix * vec4(newPosition, 1.0);
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(superquadricPosition, 1.0);
 
-    vec3 newNormal;
-    newNormal.x = - signed_pow(sin(eta), 2.0 - epsilon_1) * signed_pow(cos(omega), 2.0 - epsilon_2);
-    newNormal.y = signed_pow(cos(eta), 2.0 - epsilon_1);
-    newNormal.z = signed_pow(sin(eta), 2.0 - epsilon_1) * signed_pow(sin(omega), 2.0 - epsilon_2);
 
-    vNormal = normalMatrix * newNormal;
+    // updated normal
 
-    vec4 mvPosition = modelViewMatrix * vec4(position, 1.0);
+    vec3 superquadricNormal;
+    superquadricNormal.x = - signed_pow(sin(eta), 2.0 - epsilon_1) * signed_pow(cos(omega), 2.0 - epsilon_2);
+    superquadricNormal.y = signed_pow(cos(eta), 2.0 - epsilon_1);
+    superquadricNormal.z = signed_pow(sin(eta), 2.0 - epsilon_1) * signed_pow(sin(omega), 2.0 - epsilon_2);
+
+    vNormal = normalMatrix * superquadricNormal;
+
+    
+    // fragment shader variables
+
+    vec4 mvPosition = modelViewMatrix * vec4(superquadricPosition, 1.0);
     vViewPosition = -mvPosition.xyz;
-    vNormal = normalMatrix * normal;
-    vWorldPosition = (modelMatrix * vec4(position, 1.0)).xyz;
+    vWorldPosition = (modelMatrix * vec4(superquadricPosition, 1.0)).xyz;
 }
 `;
 
--- examples/vertex-shader/superquadric.js
+++ examples/vertex-shader/superquadric.js
@@ -10,9 +10,6 @@ import {vertexShader} from "./shader.js";
 
 export {initSuperquadric, updateUniforms};
 
-const etaBuffer = [];
-const omegaBuffer = [];
-
 
 class DummyGeometry extends THREE.BufferGeometry {
 	constructor (widthSegments = 32, heightSegments = 16) {
@@ -32,6 +29,10 @@ class DummyGeometry extends THREE.BufferGeometry {
 
 		const indices = [];
 		const vertices = [];
+
+		const etaBuffer = [];
+		const omegaBuffer = [];
+		const uBuffer = [];
 
 		// generate vertices, normals and uvs
 
@@ -54,8 +55,11 @@ class DummyGeometry extends THREE.BufferGeometry {
 				vertex.z = Math.sin(omega) * Math.sin(eta);
 				
 				vertices.push(vertex.x, vertex.y, vertex.z);
+
 				etaBuffer.push(eta);
 				omegaBuffer.push(omega);
+
+				uBuffer.push(u === 1 ? 1.0 : 0.0);
 				
 				verticesRow.push(index++);
 			}
@@ -79,6 +83,27 @@ class DummyGeometry extends THREE.BufferGeometry {
 		}
 		this.setIndex(indices);
 		this.setAttribute("position", new THREE.Float32BufferAttribute(vertices, 3));
+		this.setAttribute("eta", new THREE.Float32BufferAttribute(etaBuffer, 1));
+		this.setAttribute("omega", new THREE.Float32BufferAttribute(omegaBuffer, 1));
+		this.setAttribute("u", new THREE.Float32BufferAttribute(uBuffer, 1));
+	}
+}
+
+class SuperquadricMaterial extends THREE.ShaderMaterial {
+	constructor (parameters) {
+		super(parameters);
+
+		const baseShader = "basic";
+
+		this.type = "SuperquadricMaterial";
+
+		this.vertexShader = vertexShader;
+		this.fragmentShader = THREE.ShaderLib[baseShader].fragmentShader;
+
+		this.uniforms = THREE.UniformsUtils.merge([
+			THREE.ShaderLib[baseShader].uniforms, 
+			{epsilon_1: { value: 1.0 }, epsilon_2: { value: 1.0 }}
+		]);
 	}
 }
 
@@ -86,27 +111,11 @@ class DummyGeometry extends THREE.BufferGeometry {
 let mesh;
 
 function initSuperquadric () {
-	mesh = new THREE.Mesh();
 
-	const shader = "basic";
+	const geometry = new DummyGeometry(1028, 512);
+	const material = new SuperquadricMaterial();
 
-	const geometry = new DummyGeometry(16, 8);
-
-	geometry.setAttribute("eta", new THREE.Float32BufferAttribute(etaBuffer, 1));
-	geometry.setAttribute("omega", new THREE.Float32BufferAttribute(omegaBuffer, 1));
-
-	const uniforms = {
-		epsilon_1: { value: settings.epsilon_1 },
-		epsilon_2: { value: settings.epsilon_2 }
-	};
-
-	const shaderMaterial = new THREE.ShaderMaterial({
-		vertexShader: vertexShader,
-		fragmentShader: THREE.ShaderLib[shader].fragmentShader,
-		uniforms: THREE.UniformsUtils.merge([THREE.ShaderLib[shader].uniforms, uniforms])
-	});
-
-	mesh = new THREE.Mesh(geometry, shaderMaterial);
+	mesh = new THREE.Mesh(geometry, material);
 
 	return mesh;
 }
